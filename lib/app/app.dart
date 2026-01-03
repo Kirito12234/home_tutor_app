a2844a9d3edb514f6fa9540bd7ad8e4737f74df3
Shreedhar Chaudhary <chaudharyshreedhar78@gmail.com>
2026-01-03 08:13:43 +0545

    refactor: move app entry under lib/app

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:home_tutor/screens/splash_screen.dart';
+import 'package:home_tutor/features/splash/presentation/pages/splash_screen.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:home_tutor/app.dart';
+import 'package:home_tutor/app/app.dart';
 
 void main() {
   runApp(const MyApp());
