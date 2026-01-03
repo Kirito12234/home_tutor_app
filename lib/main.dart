6d1d98a70db71cbf2de8e26b66e365303773eb3b
Shreedhar Chaudhary <chaudharyshreedhar78@gmail.com>
2026-01-03 09:02:29 +0545

    feat(auth): init hive in main

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 import 'package:home_tutor/app/app.dart';
+import 'package:home_tutor/features/auth/data/datasources/local/auth_local_datasource.dart';
+import 'package:home_tutor/features/auth/data/models/user_hive_model.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Hive.initFlutter();
+  Hive.registerAdapter(UserHiveModelAdapter());
+  await Hive.openBox<UserHiveModel>(AuthLocalDataSource.userBoxName);
+  await Hive.openBox<String>(AuthLocalDataSource.sessionBoxName);
   runApp(const MyApp());
 }
 
