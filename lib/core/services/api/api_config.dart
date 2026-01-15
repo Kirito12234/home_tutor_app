6a2bd2e110315b0f48bd2589d0b9382168125641
Kirito12234 <kirito12234@users.noreply.github.com>
2026-01-15 13:24:54 +0545

    feat: add API base URL helper

---
+++ lib/core/services/api/api_config.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/foundation.dart';
+
+String apiBaseUrl() {
+  if (kIsWeb) {
+    return 'http://localhost:3000';
+  }
+
+  switch (defaultTargetPlatform) {
+    case TargetPlatform.android:
+      return 'http://10.0.2.2:3000';
+    default:
+      return 'http://localhost:3000';
+  }
+}
